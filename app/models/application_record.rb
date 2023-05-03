0e16827906a9b7fe7805740e1deef9b37c874ddc
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-03 09:54:50 +0000

    [Add]Sign_upページ

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,13 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :self_introduction, :sex, :profile_image])
-  end
-
 end
