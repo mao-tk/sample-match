b28b6be002e44ff1b43c0d86d71d5c31f7712969
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-03 01:40:45 +0000

    [Add]root_url設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "ba5eca396a2941bbb1c243f4ef57ae22.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
