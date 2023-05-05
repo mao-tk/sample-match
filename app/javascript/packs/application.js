99917cc58e0cdf86f00dfe4ea17e3151b8a4a1e4
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-05-05 07:14:23 +0000

    [Add]画像プレビュー機能/ログイン画面/sign_up画面

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-// import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -14,6 +14,10 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+
+
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
+
+import "./users.js"
